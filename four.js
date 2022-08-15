2f35c97e45815d04feb5d60368a53b7260a2852c
rusami <isehara.jp@coderdojo.com>
2022-08-15 21:02:54 +0900

    # to add comment

--- four.js
+++ four.js
@@ -1,3 +1,7 @@
+//
+// operator
+//
+
 let val_null = null;
 val_null = val_null ?? "initial";
 console.log("value =", val_null);       // "initial"
@@ -10,7 +14,7 @@ console.log(b_f);       // 5
 console.log(( 0 || undefined) && "hello");      // undefined
 console.log(!( 0 || undefined) && "hello");     // hello
 //
-console.log( null ? "apple" : "banana");        // banana
-console.log( null ?? "pineapple");              // pineapple
-console.log( { apple: "100 yen" }?.fruit ?? "banana");   // banana
+console.log( null ? "apple" : "banana");        // banana       ; ternary operator
+console.log( null ?? "pineapple");              // pineapple    ; "null" combined operator
+console.log( { apple: "100 yen" }?.fruit ?? "banana");   // banana  ; optional chainning operator
 //
